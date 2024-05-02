d43d83fedc0735086f83470d54e3d185ab12f2f2
PAPAT16 <timilehinsamuel505@gmail.com>
2024-05-02 12:00:02 +0100

    shopping

---
+++ Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Headset",
+    price: 50,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "headset.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Keyboard",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "keyboard.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "iphone 6",
+    price: 500,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "phone.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "laptop",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "laptop.jpg",
+  },
+];
